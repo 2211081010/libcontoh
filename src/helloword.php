28647dead3eb6286febd82fb16b9d3f2394aec48
2211081010 <hafifahazzahra520@gmail.com>
2024-05-29 09:19:34 +0700

    first commit

---
+++ src/helloword.php
@@ -0,0 +1,8 @@
+<?php
+namespace zahra\libhallo;
+
+class helloword{
+    public function sayHello($name){
+        return "Hello $name welcome to php and compposer to create your own library";
+    }
+}
